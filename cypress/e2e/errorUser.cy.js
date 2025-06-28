d4057cb2a3f16d35be10a2e5fd0b31ebc8bc978a
filip-stamenkov <stamenkov.filip@gmail.com>
2025-06-28 16:49:51 +0200

    Added case for erroring user, handled exception and failing test. Modified credentials in yaml

---
+++ cypress/e2e/errorUser.cy.js
@@ -0,0 +1,21 @@
+import shoppingList from '../fixtures/shoppingList.json'
+import loginPage from '../pages/login.js'
+
+describe('Test scenarios to verify when an erroring user logs in.', () => {
+
+    const username = Cypress.env('errorUsername')
+    const password = Cypress.env('password')
+  
+    it('Should add fleece jacked to cart and confirm (this should fail)', () => {
+
+        cy.on('fail', () => {
+            return false
+        })
+
+        cy.login(username, password)
+        loginPage.verifyLoggedIn()
+        cy.populateCart(shoppingList.errorScenario)
+    
+    });
+
+});
