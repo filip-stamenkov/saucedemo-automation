4e6c7e2129848e48ee5fc7d66114ce5db809588d
filip-stamenkov <stamenkov.filip@gmail.com>
2025-06-28 12:20:13 +0200

    Env secrets check

--- cypress/e2e/standardUser.cy.js
+++ cypress/e2e/standardUser.cy.js
@@ -1,7 +1,9 @@
 describe('Test scenarios to verify when a standard user logs in.', () => {
   
   it.only('Should succesfully login and verify the page.', () => {
-    cy.login('standard_user', 'secret_sauce')
+    const username = Cypress.env('username')
+    const password = Cypress.env('password')
+    cy.login(username, password)
   });
 
 });
