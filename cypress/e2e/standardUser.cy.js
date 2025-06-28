76cedc7c80c388c0352056179aaf43006ffc17f7
filip-stamenkov <stamenkov.filip@gmail.com>
2025-06-28 12:27:13 +0200

    User/Passowrd storage check

--- cypress/e2e/standardUser.cy.js
+++ cypress/e2e/standardUser.cy.js
@@ -1,8 +1,9 @@
 describe('Test scenarios to verify when a standard user logs in.', () => {
-  
-  it.only('Should succesfully login and verify the page.', () => {
+
     const username = Cypress.env('username')
     const password = Cypress.env('password')
+  
+  it.only('Should succesfully login and verify the page.', () => {
     cy.login(username, password)
   });
 
